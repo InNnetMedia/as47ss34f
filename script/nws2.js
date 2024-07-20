1b8a4d13478b6d17083971d589e237ba02596f02
Sam Anderson <159704859+InNnetMedia@users.noreply.github.com>
2024-07-20 22:20:31 +0200

    Update nws2.js

--- script/nws2.js
+++ script/nws2.js
@@ -9,7 +9,7 @@ console.log('Window location is '+window.location);
 autoImg.forEach((element,index) => {
     let path = element.src;
     element.src = `pages/news/news-images copy/aviation/tp-nws${index}.${imageFormat}`;
-    
+    if(window.path == )
 })
 
 
