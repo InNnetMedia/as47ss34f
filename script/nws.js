8f555bf191b77c00ac59753dfd3cc3d0931f103d
Sam Anderson <159704859+InNnetMedia@users.noreply.github.com>
2024-07-20 21:02:58 +0200

    Update nws.js

--- script/nws.js
+++ script/nws.js
@@ -23,7 +23,7 @@ function someThing(step2){
                 console.log(link);
                 resolve(link);
                 // Resolve the promise with the link value
-                window.location = 'pages/news.html';
+                window.location = 'as47ss34f/pages/news.html';
             });
         });
         
